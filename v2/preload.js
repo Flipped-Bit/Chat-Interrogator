32fae7fef6a615818a7c7fe96e9a8db74b15e127
Flipped-Bit <Flipped_bit@outlook.com>
2022-11-20 16:57:16 +0000

    Fixed edit issues

--- v2/preload.js
+++ v2/preload.js
@@ -28,6 +28,12 @@ function setupButtons() {
       setupChatlistener();
     }
 
+    // disable layout editing
+    var layoutEditButtons = document.querySelectorAll('button[class="edit"]')
+    Array.from(layoutEditButtons).forEach(btn => {
+      btn.disabled = !btn.disabled;
+    });
+
     // set connection state and update UI
     isConnected = ! isConnected;
     e.target.innerText = isConnected ? "Disconnect" : "Connect";
